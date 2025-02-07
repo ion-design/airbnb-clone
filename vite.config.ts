a5bd4ab5096e7d5baa19fd660ae2747188ab197e
Samuel Barnholdt <samuel.barnholdt@gmail.com>
2025-02-07 13:58:40 -0800

    m

--- vite.config.ts
+++ vite.config.ts
@@ -4,28 +4,20 @@ import tsConfigPaths from "vite-tsconfig-paths";
 import ionBabelPlugin from "./ion-babel-plugin";
 
 export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
 
   // Debug logging
   console.log('🔍 Vite Config - Environment variables:');
   console.log('MODE:', mode);
-  console.log('VITE_ENABLE_ION_JSX_SRC:', env.VITE_ENABLE_ION_JSX_SRC);
-  console.log('Type:', typeof env.VITE_ENABLE_ION_JSX_SRC);
-  console.log('Evaluated:', env.VITE_ENABLE_ION_JSX_SRC === 'true');
 
   // Create plugin options object
   const ionPluginOptions = {
-    root: process.cwd(),
-    enableIonJsxSrc: env.VITE_ENABLE_ION_JSX_SRC === 'true'
+    enableIonJsxSrc: true 
   };
   
   console.log('🔧 Ion Babel Plugin Options:', ionPluginOptions);
 
   return {
     // Expose env to the client
-    define: {
-      'process.env.VITE_ENABLE_ION_JSX_SRC': JSON.stringify(env.VITE_ENABLE_ION_JSX_SRC)
-    },
     plugins: [
       react({
         babel: {
