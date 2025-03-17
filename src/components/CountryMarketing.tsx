3341ff4c87f847de427fa77246f8526e3b1a490d
Samuel Barnholdt <samuel.barnholdt@gmail.com>
2025-03-17 16:37:12 -0700

    Updating section background to white

--- src/components/CountryMarketing.tsx
+++ src/components/CountryMarketing.tsx
@@ -42,7 +42,7 @@ const CountryMarketing: FC = () => {
   ];
 
   return (
-    <section className="mt-16 rounded-3xl bg-black">
+    <section className="mt-16 rounded-3xl bg-white">
       <div className="flex justify-between items-center mb-8">
         <div>
           <h2 className="text-3xl font-bold text-rose-500">
