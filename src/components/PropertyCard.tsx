f146d2e1fdda4c89a0c93c15b0e55e16bf0ee16c
Samuel Barnholdt <samuel.barnholdt@gmail.com>
2025-03-21 13:15:19 -0700

    Changing description text color to orange

--- src/components/PropertyCard.tsx
+++ src/components/PropertyCard.tsx
@@ -32,8 +32,8 @@ const PropertyCard: FC<PropertyCardProps> = ({
           <span className="text-yellow-500">★</span> {rating.toFixed(1)}
         </span>
       </div>
-      <p className="text-gray-500">{distance}</p>
-      <p className="text-gray-500">{dates}</p>
+      <p className="text-orange-500">{distance}</p>
+      <p className="text-orange-500">{dates}</p>
       <p>
         <span className="font-semibold">£{price}</span> night
       </p>
