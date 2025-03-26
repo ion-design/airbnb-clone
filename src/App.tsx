57b15d7c305d482d50b9a2645af1e27afdf98e53
Samuel Barnholdt <samuel.barnholdt@gmail.com>
2025-03-26 14:59:38 -0700

    Revert "Updating property data with bedrooms and bathrooms"
    This reverts commit 631a7277f380ebb36c5d71a0713b7eeae79faf37.

--- src/App.tsx
+++ src/App.tsx
@@ -54,8 +54,7 @@ function MainContent() {
       imageUrl:
         "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
       location: "Notting Hill, London",
-      bedrooms: 2,
-      bathrooms: 1,
+      distance: "2 kilometers away",
       dates: "Aug 10-15",
       price: 750,
       rating: 4.88,
@@ -64,8 +63,7 @@ function MainContent() {
       imageUrl:
         "https://images.unsplash.com/photo-1613490493576-7fde63acd811?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80",
       location: "Shoreditch, London",
-      bedrooms: 3,
-      bathrooms: 2,
+      distance: "3 kilometers away",
       dates: "Jul 20-25",
       price: 850,
       rating: 4.96,
@@ -74,8 +72,7 @@ function MainContent() {
       imageUrl:
         "https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
       location: "Chelsea, London",
-      bedrooms: 1,
-      bathrooms: 1,
+      distance: "4 kilometers away",
       dates: "Aug 1-6",
       price: 675,
       rating: 4.92,
@@ -84,8 +81,7 @@ function MainContent() {
       imageUrl:
         "https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2075&q=80",
       location: "Kensington, London",
-      bedrooms: 4,
-      bathrooms: 3,
+      distance: "20 kilometers away",
       dates: "Jul 14-19",
       price: 950,
       rating: 4.98,
