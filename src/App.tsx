6536fd6e609efb9c7a6f61a1b9d9d182572c2eca
Samraaj Bath <samraaj100@gmail.com>
2025-03-27 16:28:24 -0400

    Adding TripDetail route

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import PropertyCard from "./components/PropertyCard";
 import Footer from "./components/Footer";
 import CountryMarketing from "./components/CountryMarketing";
 import Trips from "./pages/Trips";
+import TripDetail from "./pages/TripDetail";
 // Logos and Icons
 import AirbnbLogo from "./assets/airbnb.svg";
 // Types
@@ -140,6 +141,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MainContent />} />
           <Route path="/trips" element={<Trips />} />
+          <Route path="/trip/:id" element={<TripDetail />} />
         </Routes>
       </div>
     </Router>
