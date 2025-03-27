7513146dfac9e6bef5b18eb6319304e7f39362dc
Samraaj Bath <samraaj100@gmail.com>
2025-03-27 14:35:45 -0400

    Adding bedrooms and bathrooms to property data

--- src/App.tsx
+++ src/App.tsx
@@ -54,7 +54,8 @@ function MainContent() {
       imageUrl:
         "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
       location: "Notting Hill, London",
-      distance: "2 kilometers away",
+      bedrooms: 2,
+      bathrooms: 1,
       dates: "Aug 10-15",
       price: 750,
       rating: 4.88,
@@ -63,7 +64,8 @@ function MainContent() {
       imageUrl:
         "https://images.unsplash.com/photo-1613490493576-7fde63acd811?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80",
       location: "Shoreditch, London",
-      distance: "3 kilometers away",
+      bedrooms: 3,
+      bathrooms: 2,
       dates: "Jul 20-25",
       price: 850,
       rating: 4.96,
@@ -72,7 +74,8 @@ function MainContent() {
       imageUrl:
         "https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
       location: "Chelsea, London",
-      distance: "4 kilometers away",
+      bedrooms: 1,
+      bathrooms: 1,
       dates: "Aug 1-6",
       price: 675,
       rating: 4.92,
@@ -81,7 +84,8 @@ function MainContent() {
       imageUrl:
         "https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2075&q=80",
       location: "Kensington, London",
-      distance: "20 kilometers away",
+      bedrooms: 4,
+      bathrooms: 3,
       dates: "Jul 14-19",
       price: 950,
       rating: 4.98,
